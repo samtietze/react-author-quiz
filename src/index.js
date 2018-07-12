61040d0eed6d2e423cfe2976cccbac5ed26d2a21
Sam <sam.tietze@spr.com>
2018-07-12 16:17:22 -0500

    Create data object for turn function to receive

--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ import AuthorQuiz from './AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
 
 const authors = [
-  { name: 'Mark Twain' },
+  {
+    name: 'Mark Twain',
+    imageUrl: 'images/authors/marktwain.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['The Adventures of Huckleberry Finn'],
+  },
 ];
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+const state = {
+  turnData: {
+    author: authors[0],
+    books: authors[0].books,
+  },
+};
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById('root'));
 registerServiceWorker();
